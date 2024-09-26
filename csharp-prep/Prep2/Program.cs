512612da0330eef2081de857dfac0beadc0f0b00
HelamD <helampathway@gmail.com>
2024-09-26 19:31:46 -0300

    Your grade is A

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,60 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+            
+
+                Console.Write("what is your grade in porcentage?: ");
+                string userGrade = Console.ReadLine();
+                int grade = int.Parse(userGrade);
+                
+
+                    string letter = "";
+
+
+                    if (grade >= 90 )
+                    {
+                        letter = "A";
+                    }
+
+                    else if (grade >= 80)
+                    {
+                        letter = "B";
+                    }
+                    
+
+                    else if(grade >=70 )
+                    {
+                        letter = "C";
+                    }
+
+                     else if (grade >= 60)
+                    {
+                        letter = "D";
+
+                    }
+                    
+
+                    else if (grade < 60)
+                    {
+                        letter = "F";
+                    }
+
+                    Console.WriteLine($"your grade is {letter}");
+
+
+                    if (grade > 70)
+                    {
+                        Console.WriteLine("You passed the course, YEAAAH!");
+                    }
+
+                     else if (grade < 60)
+                    {
+                        Console.WriteLine("sorry you did not passed the course");
+                    }
+
+
+
+
+
     }
 }
