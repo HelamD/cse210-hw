b8b76ff0e6034de7d51144481e65ce25335d294e
HelamD <helampathway@gmail.com>
2024-09-27 17:26:11 -0300

    The largest Number

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,62 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
-    }
+       Console.WriteLine("Enter a list of numbers, type 0 when finished");
+
+       
+       List<int> numbers = new List<int>();
+
+        int number = -1;
+
+       while (number != 0)
+       
+        {
+
+            Console.Write("Enter a number ");
+            string answer = Console.ReadLine();
+            int newAnswer = int.Parse(answer);
+
+            
+            if (newAnswer != 0)
+            {
+                numbers.Add(newAnswer);
+            }
+
+            else
+            {
+                break;
+            }
+
+        }
+        Console.WriteLine("The numberes you entered:");
+        foreach (int num in numbers)
+        {
+                Console.WriteLine(num);
+        }
+
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+         Console.WriteLine($"The sum of the numbers is: {sum}");
+
+         double average = (double)sum / numbers.Count;
+          Console.WriteLine($"The average of the numbers is: {average}");
+
+        if (numbers.Count > 0)
+        {
+                int largest = numbers[0];
+                foreach (int num in numbers)
+                {
+                        if (num > largest)
+                        {
+                            largest = num;
+                        }
+                 }  
+
+                Console.WriteLine($"The largest number is: {largest}");
+     }
+    }           
+
 }
