faf9fd095f010c9dc2b6157eb527f96ef60eadeb
HelamD <helampathway@gmail.com>
2024-09-26 23:56:16 -0300

    magic number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,41 @@ using System;
 class Program
 {
     static void Main(string[] args)
+
+       
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        Console.Write("What is the magic number?: ");
+        string magic = Console.ReadLine();
+        int numberMagic = int.Parse(magic);
+
+        int numberGuess = -1;
+
+
+        while (numberGuess != numberMagic)
+     {
+            Console.Write("What is your guess?: "); 
+            string guess = Console.ReadLine();
+            numberGuess = int.Parse(guess);
+
+            
+    
+            if (numberGuess > numberMagic)
+            {
+                Console.WriteLine("Too high");
+            }   
+
+
+            else if  (numberGuess < numberMagic)
+            {
+                Console.WriteLine("Too low");
+            } 
+
+
+            else if  (numberGuess == numberMagic)
+            {
+                Console.WriteLine("You got it");
+            } 
+
+        }
+    }             
 }
