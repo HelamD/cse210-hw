38bdac70b84c69bcb26b90ed63266d8afb8cd87b
HelamD <helampathway@gmail.com>
2024-10-27 16:11:17 -0300

    spinner out of range

--- prove/Develop05/activity.cs
+++ prove/Develop05/activity.cs
@@ -46,6 +46,8 @@ class Activity
             animationString.Add("\\");
             animationString.Add("|");
             animationString.Add("-");
+            animationString.Add("\\");
+            animationString.Add("|");
         }
 
 
